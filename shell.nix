fe8ec28bfd0025de0b2fff7bd11714e43f394969
David Reed <david@ktema.org>
2023-11-09 18:45:24 -0700

    Add yq to dependencies

--- shell.nix
+++ shell.nix
@@ -1,4 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [ zola just python311Packages.weasyprint inotify-tools ];
+    nativeBuildInputs = with pkgs.buildPackages; [ 
+      zola just python311Packages.weasyprint inotify-tools yq
+    ];
 }
